3ab30a2a5861a7ca8319e89df2aedf027649cdf8
Sunbase-ai <farouk0021@gmail.com>
2023-04-28 11:26:52 +0100

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int _islower(int c) {
+    if(c >= 97 && c <= 122) { // check if the ASCII value of c is between 'a' and 'z'
+        return 1; // c is lowercase
+    } else {
+        return 0; // c is not lowercase
+    }
+}
+
+
+int main() {
+int r,a,z,e;
+r = _islower('a');
+printf("%d\n",r);
+a = _islower('A');
+printf("%d\n",a);
+z = _islower('H');
+printf("%d\n",z);
+e = _islower('l');
+printf("%d\n",e);
+return 0;
+}
