b16e393bc4d47da94b75db76fe690afbebfc77a6
Sunbase-ai <farouk0021@gmail.com>
2023-04-28 18:00:01 +0100

    ALX

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void print_diagonal(int n){
+    int i=0;
+    while (i < n)
+    {
+        int j = 0;
+        while (j < i)
+        {
+            printf(" ");
+            j++;
+        }
+        printf("\\\n");
+        i++;
+    }
+    
+}
+
+int main(){
+int lines;
+printf("enter how many lines:");
+scanf("%d", &lines);
+print_diagonal(lines);
+}
