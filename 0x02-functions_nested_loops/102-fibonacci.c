7029751593044067e96d7b42e171984ab5e28121
Sunbase-ai <farouk0021@gmail.com>
2023-04-28 18:00:27 +0100

    ALX

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int i = 0;
+    while (i < 50)
+    {if (i=0)
+    {
+        i++;
+    }
+    else
+    {
+        i += i;
+ printf("%d, ",i);
+    }
+    
+        
+    }
+}
